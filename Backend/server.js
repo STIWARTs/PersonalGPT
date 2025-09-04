84c580ed71797f8de306f6ff695cf91dc30737da
STIWARTs <stiwartsaxena477@gmail.com>
2025-09-04 23:58:49 +0530

    Using OpenAI's with npm

--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,5 @@ app.use(express.json()); //parse incoming JSON requests
 app.use(cors()); //enable CORS
 
 app.listen(PORT, () => {
-  
   console.log(`Server is running on http://localhost:${PORT}`);
 });
