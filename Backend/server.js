0eba45edd13513b1b7ff0257727681ce01ecce20
STIWARTs <stiwartsaxena477@gmail.com>
2025-09-05 23:42:24 +0530

    Using OpenAl with API endpoints

--- Backend/server.js
+++ Backend/server.js
@@ -12,3 +12,27 @@ app.use(cors()); //enable CORS
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.post("/test", async (req, res) => {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+    },
+    body: JSON.stringify({
+      model: "gpt-4o-mini",
+      messages: [{ role: "user", content: req.body.message }],
+    })
+  };
+
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", options);
+    const data = await response.json();
+    // console.log(data.choices[0].message.content); //reply from OpenAI
+    res.send(data.choices[0].message.content);
+    // res.json(data);
+  } catch (error) {
+    console.log(err);
+  }
+});
