119c15f8ded33cdc49e3ccf95f05273070c9232c
STIWARTs <stiwartsaxena477@gmail.com>
2025-09-06 00:01:16 +0530

    Connecting to Database

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import mongoose from "mongoose";  
 
 const app = express();  
 const PORT = 8080;
@@ -11,28 +12,39 @@ app.use(cors()); //enable CORS
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
 
-app.post("/test", async (req, res) => {
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-    },
-    body: JSON.stringify({
-      model: "gpt-4o-mini",
-      messages: [{ role: "user", content: req.body.message }],
-    })
-  };
-
+const connectDB = async () => {
   try {
-    const response = await fetch("https://api.openai.com/v1/chat/completions", options);
-    const data = await response.json();
-    // console.log(data.choices[0].message.content); //reply from OpenAI
-    res.send(data.choices[0].message.content);
-    // res.json(data);
-  } catch (error) {
-    console.log(err);
+    await mongoose.connect(process.env.MONGODB_URI);
+      console.log("Connecting to MongoDB..."); 
+  } catch (error) { 
+    console.log("MongoDB connection error:", error);
   }
-});
+};
+
+
+// app.post("/test", async (req, res) => {
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+//     },
+//     body: JSON.stringify({
+//       model: "gpt-4o-mini",
+//       messages: [{ role: "user", content: req.body.message }],
+//     })
+//   };
+
+//   try {
+//     const response = await fetch("https://api.openai.com/v1/chat/completions", options);
+//     const data = await response.json();
+//     // console.log(data.choices[0].message.content); //reply from OpenAI
+//     res.send(data.choices[0].message.content);
+//     // res.json(data);
+//   } catch (error) {
+//     console.log(err);
+//   }
+// });
